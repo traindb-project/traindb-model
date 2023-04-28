4f1d6290bfee34f2ea98658086c98c089784932f
Taewhi Lee <taewhi@etri.re.kr>
2023-04-28 13:35:32 +0900

    Refactor: move test dataset

--- tests/test_rspn.sh
+++ tests/test_rspn.sh
@@ -20,7 +20,7 @@ home_dir=$(dirname -- "${BASH_SOURCE-$0}")
 home_dir=$(cd -- "$home_dir/.."; pwd -P)
 
 mkdir -p $home_dir/output
-PYTHONPATH=$home_dir/models:$PYTHONPATH python $home_dir/tools/TrainDBCliModelRunner.py train RSPN $home_dir/models/RSPN.py $home_dir/tools/test_dataset/instacart_small/data.csv $home_dir/tools/test_dataset/instacart_small/metadata.json $home_dir/output/
+PYTHONPATH=$home_dir/models:$PYTHONPATH python $home_dir/tools/TrainDBCliModelRunner.py train RSPN $home_dir/models/RSPN.py $home_dir/tests/test_dataset/instacart_small/data.csv $home_dir/tests/test_dataset/instacart_small/metadata.json $home_dir/output/
 
 echo "SELECT COUNT(*) FROM order_products"
 PYTHONPATH=$home_dir/models:$PYTHONPATH python $home_dir/tools/TrainDBCliModelRunner.py infer RSPN $home_dir/models/RSPN.py $home_dir/output/ "COUNT(*)" "" ""
