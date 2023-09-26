ddcf723508b09363b32971547b7da2f908d39c3d
Taewhi Lee <taewhi@etri.re.kr>
2023-09-26 20:26:29 +0900

    Fix: freeze_support() error in Multiprocessing

--- tools/TrainDBCliModelRunner.py
+++ tools/TrainDBCliModelRunner.py
@@ -40,61 +40,65 @@ import json
 import sys
 import csv
 
-root_parser = argparse.ArgumentParser(description='TrainDB CLI Model Runner')
-subparsers = root_parser.add_subparsers(dest='cmd')
-parser_train = subparsers.add_parser('train', help='train model command')
-parser_train.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
-parser_train.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
-parser_train.add_argument('data_file', type=str, help='(str) path to .csv data file')
-parser_train.add_argument('metadata_file', type=str, help='(str) path to .json table metadata file')
-parser_train.add_argument('model_path', type=str, help='(str) path to model')
+def main():
+  root_parser = argparse.ArgumentParser(description='TrainDB CLI Model Runner')
+  subparsers = root_parser.add_subparsers(dest='cmd')
+  parser_train = subparsers.add_parser('train', help='train model command')
+  parser_train.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
+  parser_train.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
+  parser_train.add_argument('data_file', type=str, help='(str) path to .csv data file')
+  parser_train.add_argument('metadata_file', type=str, help='(str) path to .json table metadata file')
+  parser_train.add_argument('model_path', type=str, help='(str) path to model')
 
-parser_synopsis = subparsers.add_parser('synopsis', help='generate synopsis command')
-parser_synopsis.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
-parser_synopsis.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
-parser_synopsis.add_argument('model_path', type=str, help='(str) path to model')
-parser_synopsis.add_argument('row_count', type=int, help='(int) the number of rows to generate')
-parser_synopsis.add_argument('output_file', type=str, help='(str) path to save generated synopsis file')
+  parser_synopsis = subparsers.add_parser('synopsis', help='generate synopsis command')
+  parser_synopsis.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
+  parser_synopsis.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
+  parser_synopsis.add_argument('model_path', type=str, help='(str) path to model')
+  parser_synopsis.add_argument('row_count', type=int, help='(int) the number of rows to generate')
+  parser_synopsis.add_argument('output_file', type=str, help='(str) path to save generated synopsis file')
 
-parser_synopsis = subparsers.add_parser('infer', help='inference model command')
-parser_synopsis.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
-parser_synopsis.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
-parser_synopsis.add_argument('model_path', type=str, help='(str) path to model')
-parser_synopsis.add_argument('agg_expr', type=str, help='(str) aggregation expression')
-parser_synopsis.add_argument('group_by_column', type=str, help='(str) column specified in GROUP BY clause')
-parser_synopsis.add_argument('where_condition', type=str, help='(str) filter condition specified in WHERE clause')
-parser_synopsis.add_argument('output_file', type=str, nargs='?', default='', help='(str) path to save inferred query result file')
+  parser_synopsis = subparsers.add_parser('infer', help='inference model command')
+  parser_synopsis.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
+  parser_synopsis.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
+  parser_synopsis.add_argument('model_path', type=str, help='(str) path to model')
+  parser_synopsis.add_argument('agg_expr', type=str, help='(str) aggregation expression')
+  parser_synopsis.add_argument('group_by_column', type=str, help='(str) column specified in GROUP BY clause')
+  parser_synopsis.add_argument('where_condition', type=str, help='(str) filter condition specified in WHERE clause')
+  parser_synopsis.add_argument('output_file', type=str, nargs='?', default='', help='(str) path to save inferred query result file')
 
-parser_train = subparsers.add_parser('list', help='list model hyperparameters')
-parser_train.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
-parser_train.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
-parser_train.add_argument('output_file', type=str, help='(str) path to .json model hyperparameters file')
+  parser_train = subparsers.add_parser('list', help='list model hyperparameters')
+  parser_train.add_argument('modeltype_class', type=str, help='(str) modeltype class name')
+  parser_train.add_argument('modeltype_uri', type=str, help='(str) path for local model, or uri for remote model')
+  parser_train.add_argument('output_file', type=str, help='(str) path to .json model hyperparameters file')
 
-args = root_parser.parse_args()
-runner = TrainDBCliModelRunner()
-if args.cmd == 'train':
-  data_file = pd.read_csv(args.data_file)
-  with open(args.metadata_file) as metadata_file:
-    table_metadata = json.load(metadata_file)
-  runner.train_model(args.modeltype_class, args.modeltype_uri, data_file, table_metadata, args.model_path)
-  sys.exit(0)
-elif args.cmd == 'synopsis':
-  syn_data = runner.generate_synopsis(args.modeltype_class, args.modeltype_uri, args.model_path, args.row_count)
-  syn_data.to_csv(args.output_file, index=False)
-  sys.exit(0)
-elif args.cmd == 'infer':
-  aqp_result, confidence_interval = runner.infer(args.modeltype_class, args.modeltype_uri, args.model_path, args.agg_expr, args.group_by_column, args.where_condition)
-  if args.output_file == '':
-    print(aqp_result)
-  else:
+  args = root_parser.parse_args()
+  runner = TrainDBCliModelRunner()
+  if args.cmd == 'train':
+    data_file = pd.read_csv(args.data_file)
+    with open(args.metadata_file) as metadata_file:
+      table_metadata = json.load(metadata_file)
+    runner.train_model(args.modeltype_class, args.modeltype_uri, data_file, table_metadata, args.model_path)
+    sys.exit(0)
+  elif args.cmd == 'synopsis':
+    syn_data = runner.generate_synopsis(args.modeltype_class, args.modeltype_uri, args.model_path, args.row_count)
+    syn_data.to_csv(args.output_file, index=False)
+    sys.exit(0)
+  elif args.cmd == 'infer':
+    aqp_result, confidence_interval = runner.infer(args.modeltype_class, args.modeltype_uri, args.model_path, args.agg_expr, args.group_by_column, args.where_condition)
+    if args.output_file == '':
+      print(aqp_result)
+    else:
+      with open(args.output_file, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(aqp_result)
+    sys.exit(0)
+  elif args.cmd == 'list':
+    json_hyperparams_info = runner.list_hyperparameters(args.modeltype_class, args.modeltype_uri)
     with open(args.output_file, 'w') as f:
-      writer = csv.writer(f)
-      writer.writerows(aqp_result)
-  sys.exit(0)
-elif args.cmd == 'list':
-  json_hyperparams_info = runner.list_hyperparameters(args.modeltype_class, args.modeltype_uri)
-  with open(args.output_file, 'w') as f:
-    f.write(json_hyperparams_info)
-  sys.exit(0)
-else:
-  root_parser.print_help()
+      f.write(json_hyperparams_info)
+    sys.exit(0)
+  else:
+    root_parser.print_help()
+
+if __name__ == "__main__":
+  main()
