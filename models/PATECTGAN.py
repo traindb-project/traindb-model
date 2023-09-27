3f54c495428e3ee0c80dc75747461c69994e642b
Jiwon Ock <dhrwl541@gmail.com>
2023-09-27 15:36:42 +0900

    Feat:add PATE-CTGAN model

--- models/PATECTGAN.py
+++ models/PATECTGAN.py
@@ -13,17 +13,13 @@
 """
 
 import logging
-# import rdt
-# import sdv
 from TrainDBBaseModel import TrainDBSynopsisModel
 import pandas as pd
 import torch
 
-#import numpy as np
 import os
-from snsynth.pytorch.nn import PATECTGAN as ppateCTGAN
+from snsynth.pytorch.nn import PATECTGAN as snsynthPATECTGAN
 from snsynth.pytorch.pytorch_synthesizer import PytorchDPSynthesizer
-# from snsynth.pytorch.nn.ctgan.base import BaseSynthesizer
 
 LOGGER = logging.getLogger(__name__)
 
@@ -34,30 +30,24 @@ class PATECTGAN(TrainDBSynopsisModel, PytorchDPSynthesizer):
 
     def train(self, real_data, table_metadata):
         self.columns, categorical_columns = self.get_columns(real_data, table_metadata)
-        #real_data = real_data[columns]
 
         LOGGER.info("Training %s", self.__class__.__name__)
-        self.model = PytorchDPSynthesizer(1.0, ppateCTGAN(regularization='dragan'), None)
-        # self.model = sdv.tabular.CTGAN(table_metadata=table_metadata, **self.model_kwargs)
+        self.model = PytorchDPSynthesizer(1.0, snsynthPATECTGAN(**self.model_kwargs), None)
         self.model.fit(real_data, categorical_columns=real_data.columns.values.tolist()) 
 
     def save(self, output_path):
-        #self.model = BaseSynthesizer.save(output_path + '/model.pkl') # pateCTGAN에는 save 함수가 없음
         torch.save({
             'model': self.model,
             'columns': self.columns
         }, os.path.join(output_path, 'model_info.pth'))
 
     def load(self, input_path):
-        #self.model = BaseSynthesizer.load(input_path + '/model.pkl')
         saved_model = torch.load(os.path.join(input_path, 'model_info.pth'))
         self.model = saved_model['model']
         self.columns = saved_model['columns']
 
     def synopsis(self, row_count):
         LOGGER.info("Synopsis Generating %s", self.__class__.__name__)
-        #gan = PytorchDPSynthesizer.__init__
-        #self.model = PytorchDPSynthesizer(1.0, ppateCTGAN(regularization='dragan'), None)
         synthetic_data = self.model.sample(row_count)
         synthetic_data = pd.DataFrame(synthetic_data, columns=self.columns)
 
